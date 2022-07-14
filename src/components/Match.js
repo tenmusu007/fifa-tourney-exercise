7c226e341780a429c6e1e4f08d0f0e17197f0c69
adlascio <arthur.dilascio@gmail.com>
2022-07-14 09:41:47 -0700

    added matches with interaction

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Match(props) {
-  console.log(props);
+  // console.log(props);
   const matchData = props.matchData;
+  const [winner, setWinner] = useState(matchData.winner);
+  let winnerText;
+  if (winner === 0) {
+    winnerText = <h2>No winners yet!</h2>;
+  } else {
+    winnerText = (
+      <h2>
+        {matchData.winnerName} is the winner by {matchData.scoreDifference}!
+      </h2>
+    );
+  }
   return (
     <article>
       <h1>
-        {matchData.players[0]} <span>vs</span> {matchData.players[1]}
+        {matchData.player1} <span>vs</span> {matchData.player2}
       </h1>
-      <h2>
-        {matchData.winner} is the winner by {matchData.scoreDifference}!
-      </h2>
-      <h2>No winners yet!</h2>
+      {winnerText}
+      <button
+        onClick={() => {
+          matchData.winnerName = matchData.player1;
+          setWinner(matchData.players[0]);
+        }}
+      >
+        {matchData.player1}
+      </button>
+      <button
+        onClick={() => {
+          matchData.winnerName = matchData.player2;
+          setWinner(matchData.players[1]);
+        }}
+      >
+        {matchData.player2}
+      </button>
     </article>
   );
 }
