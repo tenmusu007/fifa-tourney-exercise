18c00f2a9398b5764bab24c2ca45e753a27c9095
Atsuya Tanaka <tenmusu007@gmail.com>
2022-07-14 09:41:50 -0700

    add

--- src/components/Match.js
+++ src/components/Match.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import MatchList from './MatchList';
 
 export default function Match(props) {
-  console.log(props);
-  const matchData = props.matchData;
+  console.log("log",props.matchData);
+  // const matchDataArr =Object.values(props);
+  // console.log(matchDataArr);
+  const renderMatch =props.matchData.map((value,index)=>{
+    console.log("index",index);
+    console.log("value",value);
+    return(
+      <div key ={index}>
+        {/* {value.players} */}
+      <h1 >
+        {value.players[0]} <span>vs</span> {value.players[1]}
+        </h1>
+        <h2>
+        {value.winner} is the winner by {value.scoreDifference}!
+        </h2>
+      {value.scoreDifference === 0 && <h2>No winners yet!</h2>}
+      </div>
+    ) 
+  })
+  console.log(renderMatch);
   return (
     <article>
-      <h1>
-        {matchData.players[0]} <span>vs</span> {matchData.players[1]}
-      </h1>
-      <h2>
-        {matchData.winner} is the winner by {matchData.scoreDifference}!
-      </h2>
-      <h2>No winners yet!</h2>
+        {renderMatch}
     </article>
   );
 }
--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -3,11 +3,11 @@ import Match from './Match';
 import matchesData from '../data/matchData';
 
 export default function MatchList() {
-  //   console.log('match data', matchesData);
+    // console.log('match data', matchesData);
   return (
     <section className='MatchList'>
       <h1>Match List</h1>
-      <Match matchData={matchesData[0]} />
+      <Match matchData={matchesData} />
     </section>
   );
 }
